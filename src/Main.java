efc13a0e3819f0d4ccf7f8452c2beeffd0e5a743
Thomas <Thomas@Tannenberg.online>
2022-10-09 17:46:31 +0200

    First working version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+package online.Tannenberg;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Where to?");
+        int numberTo = Integer.valueOf(scan.nextLine());
+        System.out.println("Where from?");
+        int numberFrom = Integer.valueOf(scan.nextLine());
+
+        System.out.println("Where from?");
+
+        for (int i = numberFrom; i <= numberTo; i++) {
+            System.out.println(i);
+        }
+
     }
 }
